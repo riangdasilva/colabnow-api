a30fbf8a5e5ba05ad0cdf2c74eada7dbc0149ec6
Rianito <riangabrielb5@gmail.com>
2023-08-21 09:24:17 -0300

    first commit

---
+++ src/main/java/riangdasilva/colabnowapi/ColabnowApiApplication.java
@@ -0,0 +1,13 @@
+package riangdasilva.colabnowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColabnowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColabnowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/riangdasilva/colabnowapi/ColabnowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package riangdasilva.colabnowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColabnowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
